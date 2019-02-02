1c266c7e1ed4a76b760d891e0239db4f834aea98
Joby Joseph <joby.joseph@litmus7.com>
2019-02-02 10:41:17 +0530

    using state

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { AppRegistry, Image, View } from "react-native";
+import { AppRegistry, Image, View, Text } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    // Toggle the state every second
+    setInterval(
+      () =>
+        this.setState(previousState => ({
+          isShowingText: !previousState.isShowingText
+        })),
+      1000
+    );
+  }
   render() {
-    let pic = {
-      uri:
-        "https://s3.caradvice.com.au/wp-content/uploads/2016/04/2016_mercedes-benz_gls_showroom2.jpg"
-    };
     return (
       <View>
-        <Image source={pic} style={{ width: 300, height: 150 }} />
+        {this.state.isShowingText ? <Text /> : <Text>Blinking Text</Text>}
       </View>
     );
   }
